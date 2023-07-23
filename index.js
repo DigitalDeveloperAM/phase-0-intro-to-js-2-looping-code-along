459a21dfa9f43c58f654be1f6ecca8bb3457795e
DigitalDeveloperAM <achrafmehenneche.am@gmail.com>
2023-07-23 22:51:55 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, eventName) {
+    const messages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+//const namesArray = ["Charlie", "Samip", "Ali"];
+//const eventName = "birthday";
+
+function countDown(startingNumber) {
+    while (startingNumber >= 0) {
+        console.log(startingNumber);
+        startingNumber -= 1;
+    }
+
+}
